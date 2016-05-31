60cfe8659521c949da0a714212bbaaa7f543b3e4
Fondass <sybrenbos@hotmail.com>
2016-05-31 15:40:02 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-require_once('classes/class.pdo.php');
+define("_LOGPATH_", "logs/");
 
-?>
+session_start();
+
+require_once("class.debug.php");
+include("class.page_controller.php");
+
+
+
+$database = new "";
+
+
+
+
+$myController = new FonController();
+
+$myController->fonRequestCheck();
