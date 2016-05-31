e283f858cf39f5650f1a1ba477a72db8f28cd499
Iain <iainjong@gmail.com>
2016-05-31 15:19:46 +0200

    Create class.db.php

---
+++ classes/class.db.php
@@ -0,0 +1,24 @@
+<?php
+
+//this file contains handy functions that perform database queries, please only do this by referencion to the functions in the pdo-
+//class by using the Paamayim Nekudotayim operator
+
+class databases 
+{
+	public function __construct()
+	{
+		if (PDODAO::connect() == true)
+		{
+			
+		}
+		else
+		{
+			die("NO DATABASE CONNECTION");
+		}
+	}
+	
+	//=============================================
+	
+	//TODO: add more functions here
+	
+?>
