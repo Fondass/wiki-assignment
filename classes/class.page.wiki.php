3bc3d1a0ab6473c98fee0a0e2b38d6d9d258df8b
Iain <iainjong@gmail.com>
2016-06-01 09:07:20 +0200

    Create class.page.wiki.php

---
+++ classes/class.page.wiki.php
@@ -0,0 +1,12 @@
+<?php
+	
+	//this is the base html template for a wiki page, it extends from the page class
+	
+	//make sure this is required at the places where it is needed
+	//include the classes that extend this class
+	//include('classes/');
+		
+	class Wiki extends Page
+	{
+	  //TODO: make custom functions and variables if needed
+	}
