39d1af4f68b3abec2533d2ab92ed7b76685d57eb
Iain <iainjong@gmail.com>
2016-06-01 13:14:23 +0200

    Create class.page.wiki.test.php
    New file, it is a test page

---
+++ classes/class.page.wiki.test.php
@@ -0,0 +1,31 @@
+<?php
+	
+	//this sample attempts to generate a html wiki page based upon data from the page table in the database. Later on we may try placing this 
+	//kind of functionality in a wikipage class
+
+	class Test extends Wiki
+		{
+		    var $page = array();
+                    
+                    function __construct($arr) 
+                    {
+                            if(is_array($arr))
+                            {
+                                $this->page = $arr;
+                            }
+                            else
+                            {
+                                throw new Exception('Page not found.');
+                            }
+                    }
+
+
+            //TODO: add variables and functions unique to the home page
+            
+                    function headerContent() 
+			{ 
+				echo "Article name: ".$this->page[0]['name'];
+                                echo "<br />Page content: ".$this->page[0]['content'];
+                                echo "<br />Creator id: ".$this->page[0]['users_id'];
+			}
+		}
