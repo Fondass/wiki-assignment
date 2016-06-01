f927700e7cf65d4933e83598d8bb7e81e962049f
Iain <iainjong@gmail.com>
2016-06-01 09:25:27 +0200

    Create class.page.wiki.home.php
    Test home page.

---
+++ classes/class.page.wiki.home.php
@@ -0,0 +1,10 @@
+
+<?php
+	
+	//this is a sample home page for now. I will add some links to wiki-pages here so that we can test them.
+	
+	class Home extends Wiki
+		{
+		    //TODO: add variables and functions unique to the home page
+		}
+		
