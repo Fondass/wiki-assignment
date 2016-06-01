1980e93c65817d88246a63a921ce34c7e08019e7
Iain <iainjong@gmail.com>
2016-06-01 13:15:21 +0200

    Update class.page.wiki.home.php
    Suggested edit to the header

--- classes/class.page.wiki.home.php
+++ classes/class.page.wiki.home.php
@@ -6,5 +6,10 @@
 	class Home extends Wiki
 		{
 		    //TODO: add variables and functions unique to the home page
+		    function headerContent() 
+			{ 
+                            //make sure the local link is correct when testing
+                            echo 'Hello world, welcome to the wiki.<br /> <a href="?page=test">Test page.</a>';
+			}
 		}
 		
