e815be640f825df29a5a49284a220f3ef77a2855
Iain <iainjong@gmail.com>
2016-06-02 16:10:14 +0200

    added some functions

--- classes/class.db.php
+++ classes/class.db.php
@@ -3,7 +3,7 @@
 //this file contains handy functions that perform database queries, please only do this by referencing the functions in the pdo-
 //class (not on github) by using the Paamayim Nekudotayim (::) operator
 
-class databases 
+class database
 {
 	public function __construct()
 	{
@@ -18,9 +18,9 @@ class databases
 	}
 	
 //=============================================
-	
-	//TODO: add more functions here
-	
+//TODO: add more functions here
+//=============================================
+		
         public function fonSavePageToDatabase($title, $content, $tags)
         {
             
@@ -46,28 +46,49 @@ class databases
             
             
         }
+//=============================================
+
+    public function selectPagesOnTag($tag)
+    {
+        #####this is if you want to accept a tagname instead of a tagid#####
+        #$sql = 'SELECT id FROM tags WHERE name = "'.$tag.'"';
+        #$statement = PDODAO::prepareStatement($sql);
+        #$tagids PDODAO::getArray($statement);
+        ####################################################################
         
+        $sql = 'SELECT * FROM pages JOIN pages_tags ON pages.id = pages_tags.pages_id WHERE pages_tags.tags_id = '.$tag;
+        //$statement = PDODAO::prepareStatement($sql);
+        return PDODAO::getDataArrays($sql);
+    }
+
+//=============================================    
+    
+    public function getTags()
+	{
+            $sql = 'SELECT * FROM tags';
+            return PDODAO::getDataArrays($sql);
+	}
+
 //=============================================
     
     // a function that selects an article in the database based upon it's name.
-    // there may be a better pdodao:: function to do this than getArrays
-    function selectPagesName($name)
+    // there may be a better pdodao:: function to do this than getArrays (replaced with getArray)
+    public function selectPagesName($name)
 	{
-		$sql = 'SELECT * FROM pages WHERE name = "'.$name.'"';
-		$statement = PDODAO::prepareStatement($sql);
-		return PDODAO::getArrays($statement);
+            $sql = 'SELECT * FROM pages WHERE name = "'.$name.'"';
+            return PDODAO::getDataArray($sql);
 	}
         
 //=============================================
     
     // a test function that selects an article in the database based upon id.
-    function selectTest($id)
+    public function selectTest($id)
 	{
-		$sql = "SELECT * FROM pages WHERE id = ".$id;
-		$statement = PDODAO::prepareStatement($sql);
-		return PDODAO::getArrays($statement);
+            $sql = "SELECT * FROM pages WHERE id = ".$id;
+            $statement = PDODAO::prepareStatement($sql);
+            return PDODAO::getArrays($statement);
 	}
 
         
         
-?>
+}
