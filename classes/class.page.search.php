be20d0d3afda87f4366adfb0f9a434aa3e36191d
Iain <iainjong@gmail.com>
2016-06-03 11:24:54 +0200

    turned radiobutton into checkbox

--- classes/class.page.search.php
+++ classes/class.page.search.php
@@ -36,16 +36,9 @@ class SearchPage extends Page
         
         foreach ($this->tags as $key => $value)
         {
-            echo '<input type="radio" name="tagid" value="'.$value["id"].'">'.$value["name"].'</input><br>';
+            echo '<input type="checkbox" name="tagid[]" value="'.$value["id"].'">'.$value["name"].'</input><br>';
         }   
 
-            //<input type="hidden" name="page" value="searchresult">
-            //<input type="radio" name="tagid" value="1">java<br>
-            //<input type="radio" name="tagid" value="2">chip<br>
-            //<input type="radio" name="tagid" value="3">scombridae<br>
-            //<input type="radio" name="tagid" value="4">mammal<br>
-            //<input type="radio" name="tagid" value="5">nerd<br>
-            //<input type="radio" name="tagid" value="6">error
         echo '</fieldset>
             <input type="submit" name="submit" value="Commit">
             </form></div>'; 
