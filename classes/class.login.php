360f1fb067ac6c356a085338972c18d3c7af60ea
Fondass <sybrenbos@hotmail.com>
2016-06-03 09:30:06 +0200

    Add files via upload

---
+++ classes/class.login.php
@@ -0,0 +1,90 @@
+<?php
+
+class FonLogin
+{
+    
+
+    public function __construct()
+    {
+        $this->db = new databases();
+    }
+    
+    
+    protected $username;
+    protected $password;
+    protected $db;
+
+
+    /*
+     * fonUserCheck is a function that initially runs to see if the entered vallues
+     * on the login page (class.page.wiki.login.php) are correct.
+     * It returns true when a user has succsesfully logged in, and false when a 
+     * user entered the wrong username / password.
+     */
+    
+    
+    public function fonUserCheck()
+    {
+        if (isset ($_POST["usernamefield"]))
+        {
+            $username = $_POST["usernamefield"];
+        }
+        
+        else
+        {
+            $username = "";
+        }
+        
+        if (isset ($_POST["passwordfield"]))
+        {
+            $password = $_POST["passwordfield"];
+        }
+        
+        else
+        {
+            $password = "";
+        }
+        
+        
+        $username = htmlspecialchars($username, ENT_QUOTES, "UTF-8");
+        
+        $pass = $this->db->fonCheckUserCredentials($username);
+        
+        if ($password === $pass)
+        {
+            $_SESSION["username"] = $username;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    /*
+     * fonLoggedUser is a small function that one might call upppon whenever one sees
+     * fit to check if the user is really logged in. 
+     * 
+     * This is the function to use to close of certain parts of the website:
+     * 
+     * (returns true if the user is correctly logged in, and false if not)
+     */
+    
+
+    public function fonLoggedUser()
+    {
+        return (isset($_SESSION["username"]) != "");
+    }
+    
+   /*
+    * function to call uppon to log the user out. (alternatively, one can simply 
+    * enter session_destroy() whenever.
+    */
+    
+    
+    public function fonUserLogout()
+    {
+        session_destroy();
+    }
+    
+}
---
+++ classes/class.page.wiki.login.php
@@ -0,0 +1,87 @@
+<?php
+
+
+require_once("class.page.wiki.wikipage.php");
+include_once("class.login.php");
+require_once("class.debug.php");
+
+session_start();
+
+class FonLoginPage extends Wikipage
+{
+    /*
+     * $this->user means the active user trying to log in or is already logged in
+     * for the duration of the session. $this-user is used in any scripts that need
+     * to validate if the user is a valid user.
+     */
+    
+    
+    
+    /*
+     * fonShowLogin is a function that displays the form for logging in.
+     * the register button is currently out of order.
+     */
+
+    
+    public function fonShowLogin() 
+    {
+        echo '<form method="POST">
+            <fieldset><legend>User Login</legend>
+            <input type="text" name="usernamefield" placeholder="Username">
+            <br>
+            <input type="text" name="passwordfield" placeholder="Password">
+            <br>
+            <input type="submit" name="loginsubmit" value="Login">
+            <input type="button" name="registerbutton" value="Register">
+            </fieldset></form>';
+    }
+      
+    /*
+     * bodyContent decides what to pressent to the user uppon visiting
+     * the login page and displays either:
+     * 
+     * if: a message or whatever if the user was already logged in and revisiting the page.
+     * 
+     * elseif: a message or whatever if the user just logged in.
+     * 
+     * 2cnd elseif: a message or thing if the user failed to log in.
+     * 
+     * else: a function that runs when the user first arives at this page without having logged in,
+     * in this case the ShowLogin funciton that displays a login form.
+     */
+    
+    public function bodyContent()
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "GET" && $this->user->fonLoggedUser() === true)
+        {
+            echo '<p> Welcome Back pall<p>';
+            Debug::writeToLogFile($this->pagename);
+        }
+        
+        elseif ($_SERVER["REQUEST_METHOD"] === "POST" && $this->user->fonUserCheck() === true)
+        {
+            echo 'Welcome Back bud';
+        }
+        
+        elseif ($_SERVER["REQUEST_METHOD"] === "POST" && $this->user->fonUserCheck() !== true)
+        {
+            echo 'The princess is in another castle';
+        }
+        
+        else
+        {
+            $this-> fonShowLogin();
+        }
+    }
+}
+
+$page = new FonLoginPage();
+
+
+
+
+
+$thing = new FonLogin();
+
+
+$page->bodyContent();
