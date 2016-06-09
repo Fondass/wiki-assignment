9d196fe1cd818788c8678159e311a0f21736a9de
Iain <iainjong@gmail.com>
2016-06-09 11:29:56 +0200

    added search class

--- classes/class.page.searchresult.php
+++ classes/class.page.searchresult.php
@@ -1,31 +1,28 @@
 <?php
 	
-	//this is a simple search result page. It searches a page based upon an inserted tag and then it returns something.
-        //it also does the HTML, but we could put all search functionality into it's own class one day
-
-        require_once("class.page.php");
-        require_once("class.db.php");
-        //require_once("class.pdo.php");
+require_once("class.search.php");
 
 //================================================
 
-    class Searchresult extends Wiki
-    {
-        var $tag = array();
-        var $pages = array();
-        var $db;
-            
+class Searchresult extends Wiki
+{
+    var $tag = array();
+    var $db;
+    var $search;
+    var $user;
+    var $title;
         
 //================================================         
     //takes a pagename as a parameter
-
-    public function __construct($tag, $db, $user) 
+    public function __construct($tag, $db, $user, $title = false) 
     {
         if (true)//(is_array($tag))
         {
             $this->tag = $tag;
+            $this->title = $title;
             $this->db = $db;
             $this->user = $user;
+            $this->search = new Search();
         }
         else
         {
@@ -34,46 +31,10 @@
     }
 
 //================================================         
-    //returns an array of pages corresponding to one tag. I am using way too many functions to do this, may need to be reduced
-
-    protected function searchPagesOnTags($val) 
-    {
-        $this->pages = $this->db->selectPagesOnTag($val);
-        return $this->pages;
-    }
-    
-//================================================    
-    //prints list of pages corresponding for each tag for each tag in the tag array
-    protected function testContent()
-    {
-        $pages = array();
-        
-        foreach($this->tag as $foo => $bar)
-        {
-            $bar = strip_tags(htmlspecialchars($bar, ENT_QUOTES, "UTF-8"));
-            $this->searchPagesOnTags($bar);
-            foreach($this->pages as $key => $value)
-            {
-                $pages[] = $value["name"];
-                //echo '* <a href="?page=wikipage&id='.$value["name"].'">'.$value["name"]."</a><br />";
-            }
-        }
-        
-        $newpages = array_merge(array_flip(array_flip($pages)));
-
-        Debug::writeToLogFile("you just preformed a search");
-        
-        foreach ($newpages as $key => $value)
-        {
-            echo '* <a href="?page=wikipage&id='.$value.'">'.$value."</a><br />";
-        }
-    }
-//================================================         
-//returns some pages
-    
+    //returns some pages    
     function bodyContent()
     {
-        $this->testContent();
+        $this->search->seekTitleTag($this->db, $this->tag, $this->title);
     }
-    
 }
+
