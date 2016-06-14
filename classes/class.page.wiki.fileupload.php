444a6986613dcc6a87722a329ad1b11a0797fbba
Iain <iainjong@gmail.com>
2016-06-14 13:06:41 +0200

    Create class.page.wiki.fileupload.php

---
+++ classes/class.page.wiki.fileupload.php
@@ -0,0 +1,18 @@
+<?php
+
+class FileUpload extends Wiki
+{
+        
+    function bodyContent() 
+    { 
+        if ($_SERVER["REQUEST_METHOD"]==="GET")
+        {
+            echo '<form action="helpers/upload.php" method="post" enctype="multipart/form-data">
+                Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+                </form>';
+        }    
+        
+
+}}
