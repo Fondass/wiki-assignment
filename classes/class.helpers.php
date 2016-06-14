b6b27a94b0fa79106c4bc36b6f45bfa0d5683202
Iain <iainjong@gmail.com>
2016-06-14 13:18:36 +0200

    Create class.helpers.php

---
+++ classes/class.helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * this helper class contains usefull functions
+ */
+
+class Helpers
+{
+    static function arrayChecker($keyname, $defaultreturn = false)
+    {
+        if ($_SERVER["REQUEST_METHOD"]==="POST")
+        {
+            if (isset($_POST[$keyname]))
+            {
+                return $_POST[$keyname];
+            }
+            else
+            {
+                
+                echo "notset";
+                return $defaultreturn;
+            }
+        }
+        if ($_SERVER["REQUEST_METHOD"]==="GET")
+        {
+            if (isset($_GET[$keyname]))
+            {
+                return $_GET[$keyname];
+            }
+            else
+            {
+                return $defaultreturn;
+            }
+        }
+        return $defaultreturn;
+    }
+}
