4fdcdc2b00ca36ec446245cc9ad109e8447a8a7f
Iain <iainjong@gmail.com>
2016-06-14 13:07:28 +0200

    Update class.page.wiki.register.php

--- classes/class.page.wiki.register.php
+++ classes/class.page.wiki.register.php
@@ -7,21 +7,36 @@ class Register extends Wiki
     
     public function bodyContent()
     {
-        if (isset($_POST["registerbutt"]))
+        if ((isset($_POST["registerbutt"])) && $_SESSION['register'] === true)
         {
-            $this->showRegFormFilled();
+            //var_dump($_SESSION['register']);
+            $_SESSION['register'] = false;
+            if(isset($_POST["captcha"]) && $_POST["captcha"]!="" && $_SESSION["code"]==$_POST["captcha"])
+            {
+                echo 'captcha correct!';
+                $this->showRegFormFilled();
+            }
+            else
+            {
+                echo 'captcha invalid';
+            }
+            //var_dump($_SESSION['register']);
         }
+        
         else
         {
+            //this boolean limits the a
+            $_SESSION['register'] = true;
             $this->showRegisterForm();
         }
     }
 
     protected function showRegisterForm() 
     { 
-        $reg = '<h1>Register</h1>';
+        $reg = '<b>Register</b>';
 
-        $reg .= '<form name="register" action="" method="POST">';
+        $reg .= '<form name="register" action="" method="POST">'
+                . '<input type="hidden" name="page" value="register">';
 
         $reg .= '
                 Username: 
@@ -31,6 +46,9 @@ class Register extends Wiki
         $reg .= '
                 Wachtwoord: 
                 <input type="password" name="regpw" value=""  required /><br /><br />
+                CaptCha:
+                <input name="captcha" type="text">
+                <img src="helpers/captcha.php" /><br>
                 <input type="submit" name="registerbutt" value="Register Now" /><br />
                 ';
         $reg .= '</form>';
@@ -67,14 +85,19 @@ class Register extends Wiki
     function saveUserData()
     {
         $salt = $this->makeSalt();
-                
+                 
         if (is_string($salt))
         {
             $usern = htmlspecialchars($_POST["regusername"], ENT_QUOTES, "UTF-8");
             $pasw = htmlspecialchars($_POST["regpw"], ENT_QUOTES, "UTF-8");
             $pasw .= $salt;
             $pasw = hash("sha256",$pasw);
-            $this->db->saveNewUser($usern, $pasw, $salt);
+            $result = $this->db->saveNewUser($usern, $pasw, $salt);
+            return $result;
+        }
+        else
+        {
+            return false;
         }
     }
     
@@ -82,7 +105,15 @@ class Register extends Wiki
     
     protected function showRegFormFilled() 
     { 
-        $this->saveUserData();
-        echo 'thank you so much for registering';
+        
+        $success = $this->saveUserData();
+        if ($success == true)
+        {
+            echo 'thank you so much for registering!';
+        }
+        else
+        {
+            echo 'but registration failed!';
+        }
     }
 }
