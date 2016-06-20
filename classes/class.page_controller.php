c28a31cd47fafd62b1d622bbe0096a7cfb61d141
Iain <iainjong@gmail.com>
2016-06-20 10:35:44 +0200

    more ajax things

--- classes/class.page_controller.php
+++ classes/class.page_controller.php
@@ -91,6 +91,79 @@ class FonController
                $id = htmlspecialchars($_POST["pageid"], ENT_QUOTES, "UTF-8");               
                $rater->ratingCalc($id, $score);
                break;
+               
+               case 'advanced':
+               require_once("classes/class.page.php");
+               require_once("classes/class.page.wiki.php");
+               require_once("classes/class.page.search.php");
+               $thing = new SearchPage($this->db, $this->user);
+               $thing->search->searchBox($this->db, true, true);
+               break;
+               
+               ## BELOW CODE NEEDS TO BE PUT IN IT'S OWN FUNCTIONS. WORK IN PROGRESS! ##
+           
+               case 'more':                              
+               session_start();
+               $_SESSION['searchresults'] += 5;
+               if (($_SESSION['searchresults'] + 5) < count($_SESSION['searchcache']))
+               {
+                    for($i = 0; $i < 5; $i++)
+                    {
+                        $a = $i + $_SESSION['searchresults'];
+                        echo '* <a href="?page=wikipage&id='.$_SESSION['searchcache'][$a].'">'.$_SESSION['searchcache'][$a]."</a><br />";
+                    }
+
+                    echo '<br /><button id="less">Previous</button></div>';
+                    echo '<button id="more">Next</button></div>';
+               }
+               else
+               {
+                    for($i = 0; $i < 5; $i++)
+                    {
+                        $a = $i + $_SESSION['searchresults'];
+                        echo '* <a href="?page=wikipage&id='.$_SESSION['searchcache'][$a].'">'.$_SESSION['searchcache'][$a]."</a><br />";
+                                                
+                        if (($a + 1) == count($_SESSION['searchcache']))
+                        {
+                            break;
+                        }
+                    }
+                    echo '<br /><button id="less">Previous</button></div>';
+               }
+
+               //$jsonarray = json_encode($_SESSION['searchcache']);
+               //var_dump ($jsonarray);
+               break;
+            case 'less':
+                              
+               session_start();
+               $_SESSION['searchresults'] -= 5;
+               if ($_SESSION['searchresults'] < count($_SESSION['searchcache']))
+               {
+                    for($i = 0; $i < 5; $i++)
+                    {
+                        $a = $i + $_SESSION['searchresults'];
+                        echo '* <a href="?page=wikipage&id='.$_SESSION['searchcache'][$a].'">'.$_SESSION['searchcache'][$a]."</a><br />";
+                    }
+
+                    echo '<br />';
+                    if ($_SESSION['searchresults'] >= 1)
+                    {
+                        echo '<button id="less">Previous</button></div>';
+                    }
+                    echo '<button id="more">Next</button></div>';
+               }
+               else
+               {
+                   echo 'whoah whoah out of bounds!!!';
+               }
+
+               //$jsonarray = json_encode($_SESSION['searchcache']);
+               //var_dump ($jsonarray);
+               break;
+               
+               ## ABOVE CODE NEEDS TO BE PUT IN IT'S OWN FUNCTIONS. WORK IN PROGRESS! ##
+               
         }      
     }
 
