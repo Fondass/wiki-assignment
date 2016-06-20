e7c36a97088e85f8fe00cc4c02a54a574b88cefa
Fondass <sybrenbos@hotmail.com>
2016-06-20 10:19:58 +0200

    Add files via upload

--- classes/class.helpers.php
+++ classes/class.helpers.php
@@ -6,7 +6,13 @@
 
 class Helpers
 {
-    static function arrayChecker($keyname, $defaultreturn = false)
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function arrayChecker($keyname, $defaultreturn = false)
     {
         if ($_SERVER["REQUEST_METHOD"]==="POST")
         {
@@ -32,5 +38,13 @@ class Helpers
             }
         }
         return $defaultreturn;
+    }
+      
+//==================================================================    
+    
+    public function specChars($string)
+    {
+        htmlspecialchars($string ,ENT_QUOTES, "UTF-8");
+        return $string;
     }
 }
