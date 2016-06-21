bcf952bbfb76ace35878bd5d377738a187211230
Iain <iainjong@gmail.com>
2016-06-21 09:16:36 +0200

    Update class.page.wiki.fileupload.php

--- classes/class.page.wiki.fileupload.php
+++ classes/class.page.wiki.fileupload.php
@@ -6,10 +6,24 @@ class FileUpload extends Wiki
     { 
         if ($_SERVER["REQUEST_METHOD"]==="GET")
         {
-            echo '<form action="helpers/upload.php" method="post" enctype="multipart/form-data">
+            
+            echo '<form method="post" enctype="multipart/form-data">
+                <input type="hidden" name="page" value="loadfile">
                 Select image to upload:
                 <input type="file" name="fileToUpload" id="fileToUpload">
                 <input type="submit" value="Upload Image" name="submit">
                 </form>';
         }
-}}
+        
+        //action="helpers/upload.php"
+        
+        if ($_SERVER["REQUEST_METHOD"]==="POST")
+        {
+            $uploader = new Fileuploader;
+            var_dump($uploader);
+            $result = $uploader->load();
+            
+
+	}		
+}
+}
